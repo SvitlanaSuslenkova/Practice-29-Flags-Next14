ba739ee4239079645704b497009f79cde28f4b2c
Svitlana Suslenkova <svitlana.suslenkova@gmail.com>
2024-07-30 10:58:24 +0200

    1

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  /* config options here */
+};
 
 export default nextConfig;
-
-module.exports = {
-  experimental: {
-    missingSuspenseWithCSRBailout: false,
-  },
-};
